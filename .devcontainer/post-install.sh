dac150d9903cf6fc08f4193cd30e157ec27e3f9e
Scott Gallagher <51385814+scottpgallagher@users.noreply.github.com>
2020-09-21 17:41:08 -0400

    Update post-install.sh

--- .devcontainer/post-install.sh
+++ .devcontainer/post-install.sh
@@ -22,3 +22,5 @@ getLatestVersion() {
 		    wget "https://releases.hashicorp.com/terraform/"$VERSION"/terraform_"$VERSION"_linux_amd64.zip"
 		    unzip "terraform_"$VERSION"_linux_amd64.zip"
 		    sudo install terraform /usr/local/bin/
+		    pip install okta_aws
+		    pip install awscli
